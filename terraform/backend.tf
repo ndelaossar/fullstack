4685d120f62d069276900ccd396a4156ad4c468b
Nayib de la Ossa <nayib.delaossa@dualbootpartners.com>
2024-08-11 21:30:43 -0500

    testing the code

---
+++ terraform/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-backend-fullstack"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "fullstack" {
+  bucket = "fullstack-challenge-${var.env_tf}"
+}
+
+resource "aws_s3_bucket_acl" "fullstack-acl" {
+  bucket = aws_s3_bucket.fullstack.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_fullstack" {
+  bucket = aws_s3_bucket.fullstack.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "fullstack" {
+  bucket = aws_s3_bucket.fullstack.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+
+}
+
+resource "aws_s3_bucket_object" "object" {
+  for_each = fileset("./build/","*")
+  bucket = aws_s3_bucket.fullstack.id
+  key    = each.value
+  source = "./build/${each.value}"
+  etag = filemd5("./build/${each.value}")
+}
