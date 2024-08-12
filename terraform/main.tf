2fc7ee7bd75c70f3528ee9071f450602349c51be
J0k3r <ndelaossar@hotmail.com>
2024-08-12 12:10:03 -0500

    update first pipeline (#1)
    * update first pipeline
    
    * update pipe
    
    * update node
    
    * dependecy
    
    * testing the code
    
    * fix pipeline
    
    * update pipeline
    
    * testing
    
    * upadate pipeline
    
    * test new pipe
    
    * added provider and versions
    
    * test working dir
    
    * format terraform
    
    * update artifact name
    
    * test again
    
    * Testing pipeline
    
    * fix fmt
    
    * fix pipeline
    
    * testing approval
    
    * test workspace
    
    * test workspace
    
    * test again
    
    * test deploy
    
    * fix code
    
    * fix fmt
    
    * test pipeline
    
    * test path
    
    * test
    
    * fix path
    
    * update main.tf
    
    * fix static web site
    
    * fix policy
    
    * fix bucket name
    
    * update terraform code
    
    * test pr
    
    * test
    
    * test
    
    * test
    
    * firx fmt
    
    * test new pipelines
    
    ---------
    
    Co-authored-by: Nayib de la Ossa <nayib.delaossa@dualbootpartners.com>

---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+resource "aws_s3_bucket" "fullstack" {
+  bucket        = "fullstack-challenge-${var.env_tf}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "fullstack" {
+  bucket = aws_s3_bucket.fullstack.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_versioning" "versioning_fullstack" {
+  bucket = aws_s3_bucket.fullstack.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "fullstack-website" {
+  bucket = aws_s3_bucket.fullstack.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.fullstack.id
+  policy = data.aws_iam_policy_document.allow_web_access.json
+}
+
+data "aws_iam_policy_document" "allow_web_access" {
+  statement {
+    sid = "PublicReadGetObject"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "${aws_s3_bucket.fullstack.arn}/*",
+    ]
+  }
+}
+
+# Sync files
+resource "aws_s3_bucket_object" "object" {
+  for_each = fileset("nodejs-build/", "**/*.*")
+  bucket   = aws_s3_bucket.fullstack.id
+  key      = each.value
+  source   = "nodejs-build/${each.value}"
+
+  depends_on = [aws_s3_bucket.fullstack]
+}
