dd3c03da20c89e6e1f77ef54e15b2c09b0981741
Nayib de la Ossa <nayib.delaossa@dualbootpartners.com>
2024-08-12 10:27:20 -0500

    fix bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "fullstack" {
 }
 
 resource "aws_s3_bucket_public_access_block" "fullstack" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.fullstack.id
 
   block_public_acls       = false
   block_public_policy     = false
